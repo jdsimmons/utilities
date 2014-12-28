8728e1abd2e3afcca405c42e8dc75ad0db80d88b
James Simmons <nicestep@gmail.com>
2014-12-27 19:24:24 -0600

    modified:   extracthrefs.py Add comments to the end of eachj generated command.

--- extracthrefs.py
+++ extracthrefs.py
@@ -8,9 +8,12 @@ previous_url = ''
 
 for link in soup.findAll('a'):
     url = link.get('href')
-    if 'watch' in url  and  url != previous_url:
+    text = unicode(link.string)
+    if text != 'None' and 'watch?v=' in url:
+        print('# ' + text)
+    if 'watch?v=' in url  and  url != previous_url:
         i = i + 1
         trimmed_url = url[:20]
-        print('youtube-dl https://www.youtube.com' +  trimmed_url)
+        print('youtube-dl https://www.youtube.com' +  trimmed_url), 
         previous_url = url
 print 'Total links is: ',  i
